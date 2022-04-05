15dc6c26b823c5d1e8a9a3d1e8772c275d3b6688
Benson0409 <83742453+Benson0409@users.noreply.github.com>
2022-04-05 21:39:02 +0800

    Home-icon 調整

--- src/Home/HomeList.js
+++ src/Home/HomeList.js
@@ -23,37 +23,49 @@ const HomeList = ({ list, navigation }) => {
     <Box>
       <FlatList
         ListHeaderComponent={
-        <HStack alignSelf="center" >
-          {/* <Button onPress={() => Toast.show({description: "已送出"})} mt="2"  width={150} h={100} mt={5} mr={5} mb={10}>
+          <>
+            <HStack alignSelf="center">
+              {/* <Button onPress={() => Toast.show({description: "已送出"})} mt="2"  width={150} h={100} mt={5} mr={5} mb={10}>
           <Image source={}></Image>
           <Text color={"white"} fontSize="14" fontWeight="600">抽獎活動</Text>
         </Button>
         <Button onPress={() => Toast.show({description: "已送出"})} mt="2" colorScheme='violet' width={150} h={100} mt={5} mb={10}>
           <Text color={"white"} fontSize="14" fontWeight="600">國北生活圈</Text>
         </Button> */}
-        
-        <TouchableOpacity w={150} h={100}  >
-          <Image source={{uri:'https://github.com/Benson0409/APP-Midterm-project/blob/master/img/Group%2077.png?raw=true'}} 
-          w={150} h={100}  
-          borderRadius={20}
-          mr={10}
-          mt={10}
-          mb={5}
-          
-           ></Image>
-        </TouchableOpacity>
-        <TouchableOpacity w={150} h={100} >
-          <Image source={{uri:'https://github.com/Benson0409/APP-Midterm-project/blob/master/img/Group%2078.png?raw=true'}} 
-          w={150} h={100}
-           mt={10}
-          borderRadius={20}>
-          
-          </Image>
-        </TouchableOpacity>
-        </HStack>
-        
-         }
-        
+
+              <TouchableOpacity w={150} h={100}>
+                <Image
+                  source={{
+                    uri: "https://github.com/Benson0409/APP-Midterm-project/blob/master/img/Group%2077.png?raw=true",
+                  }}
+                  w={150}
+                  h={100}
+                  borderRadius={20}
+                  mr={10}
+                  mt={10}
+                  mb={5}
+                  alt="Home-icon"
+                  borderColor={"black"}
+                  borderWidth={0.5}
+                />
+              </TouchableOpacity>
+              <TouchableOpacity w={150} h={100}>
+                <Image
+                  source={{
+                    uri: "https://github.com/Benson0409/APP-Midterm-project/blob/master/img/Group%2078.png?raw=true",
+                  }}
+                  w={150}
+                  h={100}
+                  mt={10}
+                  borderRadius={20}
+                  alt="Home-icon"
+                  borderColor={"black"}
+                  borderWidth={0.5}
+                />
+              </TouchableOpacity>
+            </HStack>
+          </>
+        }
         data={list}
         renderItem={renderItem}
         keyExtractor={(item) => item.photo}
