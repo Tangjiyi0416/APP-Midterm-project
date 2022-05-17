c4c4756d00c7edb08c4683aabba1d08d82ca4041
Benson0409 <83742453+Benson0409@users.noreply.github.com>
2022-05-17 20:16:02 +0800

    redux 嘗試

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { NativeBaseProvider } from "native-base";
+
 import Navigation from "./src/navigation";
 
 export default function App() {
--- src/Home/HomeData.js
+++ src/Home/HomeData.js
@@ -11,7 +11,12 @@ import {
   Center,
 } from "native-base";
 import Starbar from "../star/starabr";
-import { TouchableOpacity, activeOpacity, Platform } from "react-native";
+import {
+  TouchableOpacity,
+  activeOpacity,
+  Platform,
+  elevation,
+} from "react-native";
 const HomeData = ({ Home, navigation }) => {
   const [Like, setLike] = useState(false);
   return (
@@ -20,66 +25,86 @@ const HomeData = ({ Home, navigation }) => {
         shadowColor={"#000"}
         shadowOpacity={0.25}
         shadowRadius={3.84}
-        shadowOffset={
-          {width: 6},
-          {height: 6}
-        }
-        
-        _dark={{ bg: "#FFFAE1", borderRadius: 25 }}
+        {...(Platform.OS == "ios"
+          ? (shadowOffset = {
+              width: 6,
+              height: 6,
+            })
+          : null)}
+        elevation={6}
+        _dark={{ bg: "#3F382E", borderRadius: 25 }}
         _light={{ bg: "#FFFAE1", borderRadius: 25 }}
       >
-      <Box w={260} h={350} mb={3} >
-          <Box >  
-            <Image source={{ uri: Home.photo }} alt="store" borderTopRadius={25} w="260" h="192" opacity={80}/>
-            <Center position= "absolute"  style={{elevation:3}} marginLeft={220}  marginTop={3} >
-            <TouchableOpacity onPress={() => setLike(!Like)}>
-              {Like ? (
-                <MaterialCommunityIcons
-                  name="cards-heart"
-                  color={"#FF0C0C"}
-                  size={30}
-                />
-              ) : (
-                <MaterialCommunityIcons
-                  name="heart-outline"
-                  color={"white"}
-                  size={30}
-                />
-              )}
-            </TouchableOpacity>
-       </Center>
-            </Box>
-      <Box ml={3} mt={3} mr={2}>
-        <Text fontWeight="bold" fontSize="20" color={"black"} >
-          {Home.name}
-        </Text>
-        <Text fontSize="15"
-        color={"black"} 
-        >{Home.address}</Text>
-        <HStack>
-          <Starbar star={Home.star} />
-          <Text>
-            {Home.star != null ? (
-              <Text fontSize={18} color={"black"} >
-                {Home.star}.0 <Text color={"#808080"}>/ 5.0</Text>
+        <Box w={260} h={350} mb={3}>
+          <Box>
+            <Image
+              source={{ uri: Home.photo }}
+              alt="store"
+              borderTopRadius={25}
+              w="260"
+              h="192"
+              opacity={80}
+            />
+            <Center
+              position="absolute"
+              style={{ elevation: 3 }}
+              marginLeft={220}
+              marginTop={3}
+            >
+              <TouchableOpacity onPress={() => setLike(!Like)}>
+                {Like ? (
+                  <MaterialCommunityIcons
+                    name="cards-heart"
+                    color={"#FF0C0C"}
+                    size={30}
+                  />
+                ) : (
+                  <MaterialCommunityIcons
+                    name="heart-outline"
+                    color={"white"}
+                    size={30}
+                  />
+                )}
+              </TouchableOpacity>
+            </Center>
+          </Box>
+          <Box ml={3} mt={3} mr={2}>
+            <Text fontWeight="bold" fontSize="20">
+              {Home.name}
+            </Text>
+            <Text
+              fontSize="15"
+              _dark={{ color: "white" }}
+              _light={{ color: "#3D3D3D" }}
+            >
+              {Home.address}
+            </Text>
+            <HStack>
+              <Starbar star={Home.star} />
+              <Text>
+                {Home.star != null ? (
+                  <Text fontSize={18}>
+                    {Home.star}.0 <Text color={"#808080"}>/ 5.0</Text>
+                  </Text>
+                ) : null}
               </Text>
-            ) : null}
-          </Text>
-        </HStack>
-        </Box>
-        <Pressable
-                onPress={() => navigation.navigate("DetailScreen", Home)}
-                height={30}
-                w={100}
-                bg="#5B5449"
-                borderRadius={25}
-                mt={5}
-                mr={3}
-                alignSelf={"flex-end"}
-              >
-              <Text color={"white"} fontSize="14" pt={1} alignSelf={"center"}>詳細資訊</Text>
+            </HStack>
+          </Box>
+          <Pressable
+            onPress={() => navigation.navigate("DetailScreen", Home)}
+            height={30}
+            w={100}
+            bg="#5B5449"
+            borderRadius={25}
+            mt={5}
+            mr={3}
+            alignSelf={"flex-end"}
+          >
+            <Text color={"white"} fontSize="14" pt={1} alignSelf={"center"}>
+              詳細資訊
+            </Text>
           </Pressable>
-      </Box>
+        </Box>
       </VStack>
     </Box>
   );
